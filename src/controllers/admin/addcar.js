b01f754a31e7c9e5bc7c0bae77f04cef136b2667
rohit pandey <1080pandey@gmail.com>
2020-09-23 17:36:56 +0530

    backend

---
+++ src/controllers/admin/addcar.js
@@ -0,0 +1,63 @@
+const Newcar = require("../../models/car");
+
+const slugify = require("slugify");
+
+exports.AddCar = (req, res) => {
+
+  console.log(req.body)
+  console.log(req.files)
+
+
+
+  let carImg = null;
+  let specdtl = null;
+  let Specification = null;
+
+  req.files.map((file) => {
+    if (file.fieldname === "carimage") {
+      carImg = file.filename;
+    }
+    else if (file.fieldname === "spec") {
+      Specification = file.filename;
+    }
+
+    else {
+      specdtl = file.filename;
+    }
+  });
+
+  const new_car = new Newcar({
+    CarName: req.body.name,
+    slug: slugify(req.body.name),
+    CarImg: carImg,
+    Specification: { icon: Specification, spec: req.body.spec },
+    specdtl: specdtl,
+  });
+
+
+
+  console.log(new_car)
+
+  new_car.save((error, data) => {
+    if (data) {
+      res.status(200).json({ data: data })
+    } else {
+      res.status(400).json({ error: error })
+    }
+  })
+
+};
+
+
+
+// get car data
+// -----------------------
+
+exports.GetCar = (req, res) => {
+  Newcar.find({}, (err, data) => {
+    if (data)
+      res.status(200).json({ data: data })
+  })
+};
+
+
