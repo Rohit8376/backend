f5a469a4a00ab6f204d9b93b65db589db5256825
rohit pandey <1080pandey@gmail.com>
2020-09-24 14:40:26 +0530

    second edition

--- src/controllers/admin/addcar.js
+++ src/controllers/admin/addcar.js
@@ -1,51 +1,64 @@
 const Newcar = require("../../models/car");
 
 const slugify = require("slugify");
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = (express());
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 exports.AddCar = (req, res) => {
-
-//  console.log(req.body)
-  //console.log(req.files)
-
 
 
   let carImg = null;
   let specdtl = null;
-  let Specification = null;
+  let icons = [];
+
 
   req.files.map((file) => {
     if (file.fieldname === "carimage") {
       carImg = file.filename;
     }
     else if (file.fieldname === "spec") {
-      Specification = file.filename;
+      icons.push(file.filename)
     }
-
-    else {
+    else if(file.fieldname === "specdtl"){
       specdtl = file.filename;
     }
   });
+
+  
+  let specifications = []
+  for (i = 0; i < icons.length; i++) {
+    specifications.push({icon:icons[i], spec:req.body.spec[i]})
+  }
+
+
 
   const new_car = new Newcar({
     CarName: req.body.name,
     slug: slugify(req.body.name),
     CarImg: carImg,
-    Specification: { icon: Specification, spec: req.body.spec },
+    Specification: specifications,
+
     specdtl: specdtl,
   });
 
 
 
+console.log(new_car)
 
-  new_car.save((error, data) => {
-    if (data) {
-      res.status(200).json({ data: data })
-      console.log(new_car)
-    } else {
-	      console.log("Something wrong")	
-      res.status(400).json({ error: error })
-    }
-  })
+
+    new_car.save((error, data) => {
+     if (data) {
+       res.status(200).json({ data: data })
+       
+     } else {
+       console.log("Something wrong")	
+       res.status(400).json({ error: error })
+     }
+   })
 
 };
 
